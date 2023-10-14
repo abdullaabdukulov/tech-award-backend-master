4feaa4588ce14da850b2acb29689a2ab9d0e9183
Abdulla-Dev <abduqulovabdulla3108@gmail.com>
2023-10-14 17:03:26 +0500

    fixed

--- config/settings/base.py
+++ config/settings/base.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = BASE_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
